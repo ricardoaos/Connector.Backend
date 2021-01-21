f8e31741c19ba3fde5ea5330dd8d31a8b69f2cda
Ricardo Augusto de Oliveira Souto <ricardoaos@hotmail.com>
2021-01-21 13:53:22 -0300

    Ajustes

---
+++ Connector.Backend.API/HostedServices/MigrationHostedService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Connector.Backend.API.HostedServices
+{
+    public class MigrationHostedService : IHostedService
+    {
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+
+        public MigrationHostedService(IServiceScopeFactory serviceScopeFactory)
+        {
+            _serviceScopeFactory = serviceScopeFactory;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var scope = _serviceScopeFactory.CreateScope();
+
+            var crudDbContext = scope.ServiceProvider.GetService<ConsumoDbContext>();
+            await crudDbContext.Database.MigrateAsync();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+            => Task.CompletedTask;
+    }
+}
