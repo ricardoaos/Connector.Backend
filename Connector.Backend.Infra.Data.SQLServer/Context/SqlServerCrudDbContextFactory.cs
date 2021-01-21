4d0ed6b2370dd41f69d49edbdcaeb9921aff6b0c
thiagosarnoni <40176699+thiagosarnoni@users.noreply.github.com>
2021-01-21 14:32:46 -0300

    SQL

---
+++ Connector.Backend.Infra.Data.SQLServer/Context/SqlServerCrudDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Connector.Backend.Infra.Data.SQLServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Tnf.Runtime.Session;
+
+namespace BasicCrud.Infra.SqlServer.Context
+{
+    public class SqlServerCrudDbContextFactory : IDesignTimeDbContextFactory<SqlServerCrudDbContext>
+    {
+        public SqlServerCrudDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<CrudDbContext>();
+
+            var configuration = new ConfigurationBuilder()
+                                    .SetBasePath(Directory.GetCurrentDirectory())
+                                    .AddJsonFile($"appsettings.Development.json", false)
+                                    .Build();
+
+            var databaseConfiguration = new DatabaseConfiguration(configuration);
+
+            builder.UseSqlServer(databaseConfiguration.ConnectionString);
+
+            return new SqlServerCrudDbContext(builder.Options, NullTnfSession.Instance);
+        }
+    }
+}
